282b20082351e400e585021ea750115332112da8
fpalen <fpalen@gmail.com>
2025-11-01 19:02:46 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
     }@inputs:
     let
       # TODO: replace with your username
-      # primaryUser = "YOUR_USERNAME";
+      primaryUser = system.primaryUser;
     in
     {
       # build darwin flake using:
