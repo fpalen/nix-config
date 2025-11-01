22e6df1a2ad9479dad04b6d012f368dddc1ddb26
fpalen <fpalen@gmail.com>
2025-11-01 18:09:59 +0100

    Update home/shell.nix
    Comment for clean first run.

--- home/shell.nix
+++ home/shell.nix
@@ -1,25 +1,25 @@
 _: {
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
+  # programs.zsh = {
+    # enable = true;
+    # enableCompletion = true;
+    # autosuggestion.enable = true;
+    # syntaxHighlighting.enable = true;
 
-    shellAliases = {
-      la = "ls -la";
-      ".." = "cd ..";
-      "nix-switch" = "sudo darwin-rebuild switch --flake ~/.config/nix";
-    };
-  };
+    # shellAliases = {
+      # la = "ls -la";
+      # ".." = "cd ..";
+      # "nix-switch" = "sudo darwin-rebuild switch --flake ~/.config/nix";
+    # };
+  # };
 
-  programs.starship = {
-    enable = true;
-    settings = {
-      add_newline = false;
-      character = {
-        success_symbol = "[λ](bold green)";
-        error_symbol = "[λ](bold red)";
-      };
-    };
-  };
-}
+  # programs.starship = {
+    # enable = true;
+    # settings = {
+      # add_newline = false;
+      # character = {
+        # success_symbol = "[λ](bold green)";
+        # error_symbol = "[λ](bold red)";
+      # };
+    # };
+  # };
+# }
