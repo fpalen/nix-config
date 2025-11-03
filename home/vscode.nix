fb9aed720974cc899e2f1c1a0751da02f9b1095f
fpalen <fpalen@gmail.com>
2025-11-03 05:57:33 +0100

    more change

--- home/vscode.nix
+++ home/vscode.nix
@@ -3,8 +3,8 @@
   programs.vscode = {
     enable = true;
     profiles.default.extensions = with pkgs.vscode-extensions; [
-        bbenoist.nix
-        shd101wyy.markdown-preview-enhanced
+        "bbenoist.nix"
+        "shd101wyy.markdown-preview-enhanced"
     ];
   };
 }
