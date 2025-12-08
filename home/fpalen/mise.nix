3e4306eacd20c96c3ebad8720c403b99666597f4
fpalen <fpalen@gmail.com>
2025-12-08 06:32:05 +0100

    format

--- home/fpalen/mise.nix
+++ home/fpalen/mise.nix
@@ -1,5 +1,4 @@
-_:
-{
+_: {
   # programs.mise = {
   # enable = true;
   # enableZshIntegration = true;
--- hosts/my-macbook/configuration.nix
+++ hosts/my-macbook/configuration.nix
@@ -1,5 +1,4 @@
-_:
-{
+_: {
   # networking.hostName = "my-macbook";
 
   # host-specific homebrew casks
